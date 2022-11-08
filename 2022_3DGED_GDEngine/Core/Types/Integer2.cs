abe45f558f44522a110fdc0721f87580c03b475e
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-11-08 12:37:32 +0000

    :sparkles: Feature - Completed Integer2

--- 2022_3DGED_GDEngine/Core/Types/Integer2.cs
+++ 2022_3DGED_GDEngine/Core/Types/Integer2.cs
@@ -13,7 +13,16 @@ namespace GD.Core.Types
             this.x = x; this.y = y;
         }
 
-        //TODO - operators(==,!=,<,<=), ToString, Equals, GetHashCode, Clone
+        public static bool operator ==(Integer2 lhs, Integer2 rhs)
+        {
+            return lhs.x == rhs.X && lhs.y == rhs.Y;
+        }
+
+        public static bool operator !=(Integer2 lhs, Integer2 rhs)
+        {
+            return lhs.x != rhs.X || lhs.y != rhs.Y;
+        }
+
         public new string ToString()
         {
             return $"[{x}, {y}]";
@@ -31,6 +40,28 @@ namespace GD.Core.Types
         {
             //shallow returns a reference to the existing object
             return this;
+        }
+
+        public override bool Equals(object obj)
+        {
+            //this typecast will cause runtime exception
+            //Integer2 theInt = (Integer2)obj;
+
+            //this typecast will set theInt to null if it fails
+            Integer2 theInt = obj as Integer2;
+            if (theInt == null)
+                return false;
+
+            return x == theInt.X && y == theInt.Y;
+
+            //return obj is Integer2 integer &&
+            //       x == integer.x &&
+            //       y == integer.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
         }
     }
 }
